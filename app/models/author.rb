ca9d181e9b4498b70b5e2957f0240aefb3cab2f8
fahad11083 <fahad.anwaar111@gmail.com>
2020-08-04 11:14:30 +0500

    add some validation on association

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
-    has_many :books, dependent: :destroy
+    has_many :books, dependent: :destroy, before_add: [:check_credit_limit]
+
+    def check_credit_limit(book)
+        puts "something fishy"
+    end
 end
